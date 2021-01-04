45d3d47a84c9fb652e579fe13374eaa508302b48
Crowdbotics <team@crowdbotics.com>
2021-01-04 14:02:51 +0000

    add component 2021

---
+++ src/features/2021316789/screens/blank.js
@@ -0,0 +1,54 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+
+export default class Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = { TextInput_4: "" }
+
+  render = () => (
+    <View>
+      <Text>Sample text content</Text>
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_4}
+        onChangeText={nextValue => this.setState({ TextInput_4: nextValue })}
+      />
+      <Image
+        source={{
+          uri:
+            "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/2_2fKtSOR.jpg"
+        }}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  View_1: {},
+  Text_2: {},
+  Button_3: {},
+  TextInput_4: {},
+  Image_5: {}
+})
