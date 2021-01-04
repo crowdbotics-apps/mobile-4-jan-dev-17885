af647941e9e6f1ace7cd5c8555ab52b3376e5bf2
Crowdbotics <team@crowdbotics.com>
2021-01-04 11:01:36 +0000

    Update models

--- backend/home/migrations/0004_remove_customtext_hgjgfhj.py
+++ backend/home/migrations/0004_remove_customtext_hgjgfhj.py
@@ -6,12 +6,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0003_customtext_hgjgfhj'),
+        ("home", "0003_customtext_hgjgfhj"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='customtext',
-            name='hgjgfhj',
+            model_name="customtext",
+            name="hgjgfhj",
         ),
     ]
